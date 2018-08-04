446966fa176ff65924867dc8136314507af0901e
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-04 00:39:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  checknums = (2...(number/number)).to_a
+  checknums = (2...number).to_a
   checknums.all? do |check|
-    number/check == 0
+    return false if number % check == 0
 end
